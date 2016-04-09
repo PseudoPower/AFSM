53d4614db3a94906745f9eebf32eecb4cb65a28e
Han <findthegateopen@gmail.com>
2016-04-09 14:28:14 -0700

    update RPN example

--- examples/RPN.hs
+++ examples/RPN.hs
@@ -3,8 +3,9 @@
 module Main where
 
 import Control.AFSM
+import Data.Maybe
 
-data Op = Add | Sub | Mul | Div | L | R
+data Op = Add | Sub | Mul | Div
   deriving (Eq)
   
 instance Show Op where
@@ -12,35 +13,37 @@ instance Show Op where
   show Sub = "-"
   show Mul = "*"
   show Div = "/"
-  show L = "("
-  show R = ")"
+  
 
-data Token = Num Int | Op Op | End 
+data Token = Num Int | Op Op | L | R | End 
   deriving (Eq)
 
 instance Show Token where
   show (Num x) = show x
   show (Op op) = show op
+  show L = "("
+  show R = ")"
   show End = ""
 
 
 trans0 :: [Token] -> Token -> ([Token], [Token])
-trans0 xs End = ([], xs)
+trans0 xs End = ([End], xs)
 trans0 xs (Num x) = (xs, [(Num x)])
-trans0 xs op@(Op L) = (op:xs, [])
-trans0 xs op@(Op R) = (tail $ dropWhile ((Op L) /= ) xs, takeWhile ((Op L) /= ) xs)
+trans0 xs L = (L:xs, [])
+trans0 xs R = let (x0, x1) = span (L /= ) xs in (tail $ x1, x0)
 trans0 xs op = 
-  (op:(dropWhile f0 xs), takeWhile f0 xs)
+  (op:x1, x0)
   where
     f0 = (\x -> x == (Op Mul) || x == (Op Div))
-    
-in2post = simpleSM [] trans0
+    (x0, x1) = span f0 xs
+
+in2post = simpleSM [End] trans0
 
 -- 3 * (2 - 3) + (4 - 2 * 3)
-test1 = [Num 3, Op Mul, Op L, Num 2, Op Sub, Num 3, Op R, Op Add, Op L, Num 4, Op Sub, Num 2, Op Mul, Num 3, Op R, End]
+test1 = [Num 3, Op Mul, L, Num 2, Op Sub, Num 3, R, Op Add, L, Num 4, Op Sub, Num 2, Op Mul, Num 3, R, End]
 
 -- 3 + 4 * 2 / (1 - 5) * 2 + 3
-test2 = [Num 3, Op Add, Num 4, Op Mul, Num 2, Op Div, Op L, Num 1, Op Sub, Num 5, Op R, Op Mul, Num 2, Op Add, Num 3, End]
+test2 = [Num 3, Op Add, Num 4, Op Mul, Num 2, Op Div, L, Num 1, Op Sub, Num 5, R, Op Mul, Num 2, Op Add, Num 3, End]
 
 out1 = concat $ snd $ exec in2post test1
 out2 = concat $ snd $ exec in2post test2
@@ -51,10 +54,10 @@ f Sub x y = x - y
 f Mul x y = x * y
 f Div x y = quot x y
 
-trans1 :: [Int] -> Token -> ([Int], Int)
-trans1 xs End = if (null xs) then (xs, 0) else (tail xs, head xs)
-trans1 xs (Num x) = (x:xs, x)
-trans1 (x:y:xs) (Op o) = ((f o y x):xs, (f o y x))
+trans1 :: [Int] -> Token -> ([Int], Maybe Int)
+trans1 xs End = if (null xs) then (xs, Just 0) else ([], Just $ head xs)
+trans1 xs (Num x) = (x:xs, Nothing)
+trans1 (x:y:xs) (Op o) = ((f o y x):xs, Nothing)
 
 post2ret = execSM $ simpleSM [] trans1
 
@@ -63,16 +66,49 @@ in2ret = proc x -> do
    post2ret -< y
 
 historySM :: SM a [a]
-historySM = simpleSM [] (\xs a -> (a:xs, reverse (a:xs)))
+historySM = simpleSM [] (\xs a -> (a:xs, a:xs))
 
 lst2str:: Show a => SM [a] String
 lst2str = arr (let f = \xs -> if (null xs) then "" else (show (head xs)) ++ f (tail xs) in f)
 
+{-
 fooSM :: SM Token String
 fooSM = proc x -> do
   h <- (historySM >>> lst2str) -< x
   r <- in2ret -< x
   returnA -< (h ++ if null r then "" else "=" ++ show (last r) )
+-}
 
 getRet :: SM a b -> [a] -> [b]
 getRet sm xs = snd $ exec sm xs
+
+calc :: [Token] -> [Int]
+calc xs = catMaybes $ concat $ getRet (in2post >>> post2ret) xs
+
+isNum :: Char -> Bool
+isNum x = elem x "0123456789"
+
+parseOp :: Char -> Token
+parseOp '(' = L
+parseOp ')' = R
+parseOp '+' = Op Add
+parseOp '-' = Op Sub
+parseOp '*' = Op Mul
+parseOp '/' = Op Div
+
+
+parseStr :: String -> [Token]
+parseStr [] = [End]
+parseStr (x:xs) = 
+  if elem x ",\n" then End : (parseStr xs)
+  else if x == ' ' then parseStr xs
+  else if isNum x then
+    let (ys, zs) = span isNum xs in (Num $ read (x:ys)):(parseStr xs)
+  else if elem x "()+-*/" then
+    (parseOp x):(parseStr xs)
+  else 
+    parseStr xs
+  
+main = do
+  getContents >>= (mapM_ putStrLn).(map show).(calc.parseStr)
+  
