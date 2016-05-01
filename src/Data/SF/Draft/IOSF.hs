0e6066482fba3f4cd860f0d10787e4efb92578fb
FiveEye <findthegateopen@gmail.com>
2016-05-01 00:31:38 -0700

    update draft

--- src/Data/SF/Draft/IOSF.hs
+++ src/Data/SF/Draft/IOSF.hs
@@ -9,10 +9,13 @@
 -- Portability :  portable
 -----------------------------------------------------------------------------
 
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Data.SF.Draft.IOSF where
 
 import Control.Category
 import Control.Arrow
+import Control.Monad
 
 import Control.Concurrent
 import Control.Concurrent.STM
@@ -20,19 +23,11 @@ import Control.Concurrent.STM.TChan
 import Data.IORef
 
 import Data.SF.CoreType
-
+{-
 data IOSF a b = IOSF {
   refsf :: IORef (SF a (Event b)),
   output :: b -> IO ()
 }
-
--- data TChanSF a b = TChanSF (IOSF a b) (TChan b)
-
-newTChanSF :: SF a (Event b) -> IO (IOSF a b, TChan b)
-newTChanSF sf = do
-  o <- newBroadcastTChanIO
-  s <- newIORef sf
-  return (IOSF s (\b -> atomically $ writeTChan o b), o)
 
 runSF :: a -> SF a b -> (SF a b, b)
 runSF a (SF sf) = sf a
@@ -41,8 +36,61 @@ outputE :: Event b -> (b -> IO ()) -> IO()
 outputE NoEvent _ = return ()
 outputE (Event b) f = f b
 
-runIOSF :: IOSF a b -> IO a -> IO ()
-runIOSF (IOSF sf o) ia = do
-  a <- ia
+runIOSF :: IOSF a b -> a -> IO ()
+runIOSF (IOSF sf o) a = do
   b <- atomicModifyIORef' sf (runSF a)
   outputE b o
+-}
+
+writeList2TChan :: TChan a -> [a] -> IO ()
+writeList2TChan ch ls = atomically $ sequence_ (map (writeTChan ch) ls)
+
+outputTChan :: TChan a -> (a -> IO ()) -> IO ()
+outputTChan ch f = do
+  mych <- atomically $ dupTChan ch
+  forever $ do
+    a <- atomically $ readTChan mych
+    f a
+    
+tryOutputTChan :: TChan a -> (a -> IO ()) -> IO ()
+tryOutputTChan ch f = do
+  mych <- atomically $ dupTChan ch
+  ret <- atomically $ tryReadTChan mych
+  case ret of
+    Nothing -> return ()
+    Just a -> f a
+    
+data IOSF a b = forall t. Foldable t => IOSF (IORef (SF a (t b))) (b -> IO ())
+
+runSF :: a -> SF a b -> (SF a b, b)
+runSF a (SF sf) = sf a
+
+runIOSF :: IOSF a b -> a -> IO ()
+runIOSF (IOSF sf o) a = do
+  b <- atomicModifyIORef' sf (runSF a)
+  forM_ b o
+
+
+data TChanSF a b = TChanSF (IOSF a b) (TChan b)
+
+newTChanSF :: Foldable t => SF a (t b) -> IO (TChanSF a b)
+newTChanSF sf = do
+  o <- newBroadcastTChanIO
+  s <- newIORef sf
+  return $ TChanSF (IOSF s (\b -> atomically $ writeTChan o b)) o
+
+  
+type ThreadSF a b = (TChan a) -> IO (TChan b)
+
+fromTChanSF :: TChanSF a b -> ThreadSF a b
+fromTChanSF (TChanSF sf tb) ta = do
+  myta <- atomically $ dupTChan ta
+  forkIO $ forever $ do
+    a <- atomically $ readTChan myta
+    runIOSF sf a
+  return tb
+
+newThreadSF :: Foldable t => SF a (t b) -> IO (ThreadSF a b)
+newThreadSF sf = do
+  tsf <- newTChanSF sf
+  return $ fromTChanSF tsf
