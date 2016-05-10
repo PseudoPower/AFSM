681b93985f439a7cf4d974755b97b45ace8e72eb
FiveEye <findthegateopen@gmail.com>
2016-05-10 09:20:10 -0700

    update

--- draft/testSFM.hs
+++ draft/testSFM.hs
@@ -17,9 +17,22 @@ import Control.Monad
 import Data.SF
 import Data.SF.SFM
 import Data.Maybe
+import Data.Time.Clock
 
 
-sfm0 = idSFM :: SFM Maybe a a
 
+timerSFM :: a -> Int -> IO (SFM IO () (Maybe a))
+timerSFM a n = do
+  t <- getCurrentTime
+  simpleSFM f t
+    where
+      f t () = do
+        t' <- getCurrentTime
+        if truncate (diffUTCTime t' t) < n then return (t, Nothing)
+        else return (t', Just a)
+        
+test =  (timerSFM 1 10) >>= simpleSrcM
+      
 main = do
+  test >>= (\xs -> forM_ xs (putStrLn.show))
   return ()
