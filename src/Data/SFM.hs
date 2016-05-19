725c0e1dca62cd86b9e61e2c1a696eeeb77c773f
Han <findthegateopen@gmail.com>
2016-05-18 22:27:21 -0700

    split SFM

---
+++ src/Data/SFM.hs
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.SFM
+-- Copyright   :  (c) Hanzhong Xu, Meng Meng 2016,
+-- License     :  MIT License
+--
+-- Maintainer  :  hanzh.xu@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Stateful functions with Monad
+--
+-----------------------------------------------------------------------------
+
+module Data.SFM (
+  
+) where
+
+import Control.Category
+import Control.Arrow
+
+import Data.SFM.CoreType
+import Data.SFM.Core
+import Data.SFM.STFM
+import Data.SFM.FunctorSFM
